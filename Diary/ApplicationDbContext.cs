d377c186c3ac4bfa92f8fca6f4c34ef6d217ec57
urchin69 <urchin@onet.plgit config  --global user.email urchin@onet.plgit config  --global user.email urchin@onet.pl>
2023-10-14 21:24:33 +0200

    Błąd context.Groups

--- Diary/ApplicationDbContext.cs
+++ Diary/ApplicationDbContext.cs
@@ -1,26 +1,21 @@
-
 namespace Diary
 {
-
- using Diary.Models.Configurations;
-using Diary.Models.Domains;
-using System;
-using System.Data.Entity;
-using System.Linq;
-
-
-
+    using Diary.Models.Configurations;
+    using Diary.Models.Domains;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
 
     public class ApplicationDbContext : DbContext
     {
-
-        public ApplicationDbContext() : base("name=ApplicationDbContext")
+        public ApplicationDbContext()
+            : base("name=ApplicationDbContext")
         {
         }
+
         public DbSet<Student> Students { get; set; }
         public DbSet<Group> Groups { get; set; }
         public DbSet<Rating> Ratings { get; set; }
-
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
@@ -29,5 +24,4 @@ using System.Linq;
             modelBuilder.Configurations.Add(new RatingConfiguration());
         }
     }
-
 }
