ca6f47142df932f3068466fd89e8e43ba6abb160
urchin69 <urchin@onet.plgit config  --global user.email urchin@onet.plgit config  --global user.email urchin@onet.pl>
2023-10-15 07:25:59 +0200

    Diary1

--- Diary/ViewModels/MainViewModel.cs
+++ Diary/ViewModels/MainViewModel.cs
@@ -99,7 +99,6 @@ namespace Diary.ViewModels
                 var groups = context.Groups.ToList();
             }
 
-
             Groups = new ObservableCollection<GroupWrapper>
             {
                 new GroupWrapper { Id = 0, Name = "Wszystkie" },
